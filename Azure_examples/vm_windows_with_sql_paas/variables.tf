42caf2b6a954e28909a6d77d04229d9838fa7c61
Raul Carvajal B <raulcarvajalbustos@outlook.es>
2022-02-05 14:12:58 +0100

    Added folder for examples

---
+++ Azure_examples/vm_windows_with_sql_paas/variables.tf
@@ -0,0 +1,18 @@
+variable "admin_password" {
+  type = string
+
+}
+
+
+variable "admin_username" {
+  type = string
+
+}
+
+variable "location" {
+  type = string
+}
+
+variable "rgname" {
+  type = string
+}
