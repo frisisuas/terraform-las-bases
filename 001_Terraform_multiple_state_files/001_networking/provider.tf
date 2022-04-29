3103093d019e3376d5c665dbb880b3e366713e23
Raul Carvajal B <raulcarvajalbustos@outlook.es>
2022-04-29 14:57:40 +0200

    modified provider.tf

--- 001_Terraform_multiple_state_files/001_networking/provider.tf
+++ 001_Terraform_multiple_state_files/001_networking/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "aztfstate"
-    storage_account_name = "tfstatee793h"
+    storage_account_name = "REPLACE"
     container_name       = "tfstate"
     key                  = "networking.tfstate"
   }
